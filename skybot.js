dc023580a02facf93fdc62f7e8c8281ea46ad2e0
Constantin Clauzel <stantin04@gmail.com>
2015-08-28 12:55:23 +0200

    basic test, seems to work

--- skybot.js
+++ skybot.js
@@ -2,10 +2,27 @@
 
 var Client = require('skybot-client');
 
-var client = Client('localhost:4224');
+var client = Client('ws://127.0.0.1:4224/uav');
 
 client.onReady(function() {
   console.log('ready');
+});
+
+process.on('exit', function(code) {
+  console.log("About to exit");
+});
+
+client.definitionHandlers.attachOnce('GCSReceiver', function(definition) {
+  client.updateHandlers.attachOnce('GCSReceiver', function(uav) {
+    console.log(uav);
+  });
+  client.connection.sendRequest('GCSReceiver');
+});
+
+client.definitionHandlers.attachOnce('GCSReceiverMeta', function(definition) {
+});
+
+client.definitionHandlers.attachOnce('ManualControlCommand', function(definition) {
 });
 
 client.connect();
