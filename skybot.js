09ff4c7d043a2a55b7e460af3bbf317531c79e94
Constantin Clauzel <stantin04@gmail.com>
2015-08-29 15:25:23 +0200

    added uav watcher, detects when a uav changed during a state machine round trip, and sends an update to the dispatcher

--- skybot.js
+++ skybot.js
@@ -2,7 +2,9 @@
 
 var MODULE_NAME = 'SkybotControl';
 var MAX_UPDATELESS_TIME = 1000;
+var SKYBOT_ID = 42424242;
 
+var UAVWatcher = require('./uavwatcher');
 var StateMachine = require('./state_machine');
 var Client = require('skybot-client');
 
@@ -28,11 +30,12 @@ var states = {
 var controlTypes = {
   LOITER: 'LOITER',
   GOTO: 'GOTO',
+  IDLE: 'IDLE',
 };
 
 var SkybotDefinition = {
   name: MODULE_NAME,
-  id: 42424242,
+  id: SKYBOT_ID,
   description: 'Provides base uavobject for high-level modules and skybot-control',
   fields: [
     {
@@ -85,39 +88,8 @@ var initialSkybotValue = {
   left: 0,
   up: 0,
 };
-var skybotValue = _.cloneDeep(initialSkybotValue);
-var skybotValueDirty = false;
-var rwKeys = ['forward', 'left', 'takeoff', 'up', 'controlType'];
 
-function updateSkybotValue(uavo, allKeys, sendUpdate) {
-  // call handlers for writable keys (rwKeys) on change
-
-  var keys = allKeys ? _.keys(initialSkybotValue) : rwKeys;
-
-  var updated = false;
-  _.forEach(keys, function(key) {
-    if (_.isUndefined(uavo[key])) {
-      return;
-    }
-
-    if (skybotValue[key] != uavo[key]) {
-      var fnName = 'on' + capitalize(key);
-      var fn = eval(fnName); // :(
-
-      skybotValue[key] = uavo[key];
-      fn(uavo[key]);
-      skybotValueDirty = true;
-    }
-  });
-  return updated;
-}
-
-// holds the initial manualcontrolsettings, needed to revert it when leaving control
-var initialMCS;
-
-function capitalize(s) {
-  return s[0].toUpperCase() + s.substr(1);
-}
+var uavwatcher;
 
 /**
  *  Connection configuration and initialization
@@ -130,12 +102,15 @@ client.onReady(function() {
   client.requestHandlers.attach(MODULE_NAME, onRequest);
   client.requestValuesForUavs(['GCSReceiver', 'ManualControlSettings']).then(
     function(values) {
-      initialMCS = values[1];
+
     },
     function(errors) {
       console.log(errors);
     }
   );
+
+  uavwatcher = new UAVWatcher(client.definitionsStore);
+  uavwatcher.addOrUpdateUAV(SKYBOT_ID, initialSkybotValue);
 });
 
 process.on('exit', function(code) {
@@ -153,7 +128,8 @@ var States = {};
 States.Idle = (function() {
   return {
     start: function() {
-      updateSkybotValue(_.extend(initialSkybotValue, {status: skybotValue.status}), true);
+      var skybotValue = uavwatcher.valueForUAV(SKYBOT_ID);
+      uavwatcher.addOrUpdateUAV(SKYBOT_ID, _.extend(initialSkybotValue, {status: skybotValue.status}));
     }
   };
 })();
@@ -163,12 +139,13 @@ States.TakingOff = (function() {
   return {
     priority: 10,
     canTrigger: function() {
+      var skybotValue = uavwatcher.valueForUAV(SKYBOT_ID);
       return skybotValue.status === statuses.CONNECTED &&
              skybotValue.takeoff === true &&
              skybotValue.state === states.IDLE;
     },
     start: function() {
-      updateSkybotValue({state: states.TAKINGOFF, forward: 0, left: 0, up: 0}, true);
+      uavwatcher.addOrUpdateUAV(SKYBOT_ID, {state: states.TAKINGOFF, forward: 0, left: 0, up: 0});
       working = true;
       setTimeout(function() {
         working = false;
@@ -187,6 +164,7 @@ States.Landing = (function() {
   return {
     priority: 10,
     canTrigger: function() {
+      var skybotValue = uavwatcher.valueForUAV(SKYBOT_ID);
       return (
               (skybotValue.status === statuses.CONNECTED && skybotValue.takeoff === false) ||
               skybotValue.status === statuses.IDLE
@@ -195,7 +173,7 @@ States.Landing = (function() {
              skybotValue.state !== states.LANDING;
     },
     start: function() {
-      updateSkybotValue({state: states.LANDING, takeoff: false, forward: 0, left: 0, up: 0}, true);
+      uavwatcher.addOrUpdateUAV(SKYBOT_ID, {state: states.LANDING, takeoff: false, forward: 0, left: 0, up: 0}, true);
       working = true;
       setTimeout(function() {
         working = false;
@@ -215,6 +193,7 @@ States.Loitering = (function() {
   return {
     priority: 5,
     canTrigger: function() {
+      var skybotValue = uavwatcher.valueForUAV(SKYBOT_ID);
       return skybotValue.controlType === controlTypes.LOITER &&
              skybotValue.status === statuses.CONNECTED &&
              skybotValue.takeoff === true &&
@@ -222,7 +201,7 @@ States.Loitering = (function() {
              skybotValue.forward !== 0 && skybotValue.left !== 0 && skybotValue.up !== 0;
     },
     start: function() {
-      updateSkybotValue({state: states.LOITERING}, true);
+      uavwatcher.addOrUpdateUAV(SKYBOT_ID, {state: states.LOITERING}, true);
       working = true;
       setTimeout(function() {
         working = false;
@@ -232,7 +211,7 @@ States.Loitering = (function() {
       return working;
     },
     end: function() {
-      updateSkybotValue({forward: 0, left: 0, up: 0});
+      uavwatcher.addOrUpdateUAV(SKYBOT_ID, {forward: 0, left: 0, up: 0});
     }
   };
 
@@ -243,13 +222,14 @@ States.GoingTo = (function() {
   return {
     priority: 5,
     canTrigger: function() {
+      var skybotValue = uavwatcher.valueForUAV(SKYBOT_ID);
       return skybotValue.controlType === controlTypes.GOTO &&
              skybotValue.status === statuses.CONNECTED &&
              skybotValue.takeoff === true &&
              skybotValue.state === states.WAITING;
     },
     start: function() {
-      updateSkybotValue({state: states.GOINGTO}, true);
+      uavwatcher.addOrUpdateUAV(SKYBOT_ID, {state: states.GOINGTO}, true);
       working = true;
       setTimeout(function() {
         working = false;
@@ -267,11 +247,12 @@ States.Waiting = (function() {
   return {
     priority: 1,
     canTrigger: function() {
+      var skybotValue = uavwatcher.valueForUAV(SKYBOT_ID);
       return skybotValue.status === statuses.CONNECTED &&
              skybotValue.takeoff === true;
     },
     start: function() {
-      updateSkybotValue({state: states.WAITING, forward: 0, left: 0, up: 0}, true);
+      uavwatcher.addOrUpdateUAV(SKYBOT_ID, {state: states.WAITING, forward: 0, left: 0, up: 0}, true);
     },
   };
 })();
@@ -285,14 +266,17 @@ machine.addState(States.Waiting);
 machine.addState(States.GoingTo);
 
 function work() {
-  skybotValueDirty = false;
   machine.update();
-  if (skybotValueDirty) {
-    client.connection.sendUpdate(MODULE_NAME, skybotValue);
-  }
-
+  undirtyWatcherAndSend();
 };
 setInterval(work, 50);
+
+function undirtyWatcherAndSend() {
+  uavwatcher.forEachDirty(function(container) {
+    client.connection.sendUpdateWithId(container.objectId, container.currentValue.value);
+    container.done();
+  });
+}
 
 // should only be called by the current state
 States.currentStateEnd = function() {
@@ -302,45 +286,6 @@ States.currentStateEnd = function() {
     States.currentState = States.Idle;
   }
 };
-
-/**
- *  UAVO update change handlers
- *  return true to signal change in uavo
- */
-function onForward(v) {
-  console.log('onForward ' + v);
-  return true;
-}
-
-function onLeft(v) {
-  console.log('onLeft ' + v);
-  return true;
-}
-
-function onUp(v) {
-  console.log('onUp ' + v);
-  return true;
-}
-
-function onTakeoff(v) {
-  console.log('onTakeoff ' + v);
-  return true;
-}
-
-function onStatus(v) {
-  console.log('onStatus ' + v);
-  return false;
-}
-
-function onState(v) {
-  console.log('onState ' + v);
-  return false;
-}
-
-function onControlType(v) {
-  console.log('onControlType');
-  return false;
-}
 
 /**
  *  Watchdog
@@ -354,7 +299,8 @@ setInterval(function() {
   var time = new Date().getTime();
   if (time - lastUpdate > MAX_UPDATELESS_TIME) {
     console.log('Watchdog fired !');
-    updateSkybotValue({status: statuses.IDLE}, true, true);
+    uavwatcher.addOrUpdateUAV(SKYBOT_ID, {status: statuses.IDLE});
+    work();
     lastUpdate = -1;
   }
 }, 500);
@@ -365,16 +311,33 @@ setInterval(function() {
 function onUpdate(uavo) {
   lastUpdate = new Date().getTime();
 
+  var doWork = false;
+  var skybotValue = uavwatcher.valueForUAV(SKYBOT_ID);
   if (skybotValue.status === statuses.IDLE) {
     console.log('exit IDLE state');
-    updateSkybotValue({status: statuses.CONNECTED}, true, true);
+    skybotValue = uavwatcher.addOrUpdateUAV(SKYBOT_ID, {status: statuses.CONNECTED});
+    if (skybotValue.dirty) {
+      skybotValue.done();
+      client.connection.sendUpdateWithId(SKYBOT_ID, skybotValue.currentValue.value);
+      doWork = true;
+    }
   }
 
-  updateSkybotValue(uavo.data);
+  var container = uavwatcher.addOrUpdateUAV(uavo.objectId, uavo.data);
+  if (container.dirty) {
+    container.done();
+    doWork = true;
+  }
+
+  if (doWork) {
+    work(); // trigger a state machine round trip
+  }
 }
 
 // respond to requests
-function onRequest() {
+function onRequest(req) {
   console.log('onRequest');
-  client.connection.sendUpdate(MODULE_NAME, skybotValue);
+  var skybotValue = uavwatcher.valueForUAV(SKYBOT_ID);
+  client.connection.sendUpdateWithId(SKYBOT_ID, skybotValue.currentValue.value);
 }
+
---
+++ uavwatcher.js
@@ -0,0 +1,137 @@
+'use strict';
+
+var _ = require('lodash');
+
+function equals(o, p) {
+  var i,
+  keysO = Object.keys(o).sort(),
+    keysP = Object.keys(p).sort();
+  if (keysO.length !== keysP.length)
+    return false;//not the same nr of keys
+  if (keysO.join('') !== keysP.join(''))
+    return false;//different keys
+  for (i=0;i<keysO.length;++i) {
+    if (o[keysO[i]] instanceof Array) {
+      if (!(p[keysO[i]] instanceof Array))
+        return false;
+      //if (compareObjects(o[keysO[i]], p[keysO[i]] === false) return false
+      //would work, too, and perhaps is a better fit, still, this is easy, too
+      if (p[keysO[i]].sort().join('') !== o[keysO[i]].sort().join(''))
+        return false;
+    }
+    else if (o[keysO[i]] instanceof Date) {
+      if (!(p[keysO[i]] instanceof Date))
+        return false;
+      if ((''+o[keysO[i]]) !== (''+p[keysO[i]]))
+        return false;
+    }
+    else if (o[keysO[i]] instanceof Function) {
+      if (!(p[keysO[i]] instanceof Function))
+          return false;
+        //ignore functions, or check them regardless?
+    }
+    else if (o[keysO[i]] instanceof Object) {
+      if (!(p[keysO[i]] instanceof Object))
+        return false;
+      if (o[keysO[i]] === o) {
+        if (p[keysO[i]] !== p)
+          return false;
+      } else if (compareObjects(o[keysO[i]], p[keysO[i]]) === false)
+          return false;//WARNING: does not deal with circular refs other than ^^
+    }
+    if (o[keysO[i]] !== p[keysO[i]])//change !== to != for loose comparison
+      return false;//not the same value
+  }
+  return true;
+}
+
+class UAVContainer {
+
+  constructor(objectId, value) {
+    this.objectId = objectId;
+    this.previousValues = [];
+    this.currentValue = {value: _.cloneDeep(value), timeFrom: new Date().getTime(), timeTo: null};
+    this.dirty = true;
+  }
+
+  lastUpdate() {
+    return this.currentValue.timeFrom;
+  }
+
+  update(val) {
+    if (equals(this.currentValue.value, val)) {
+      return;
+    }
+    this.currentValue.value = _.merge(this.currentValue.value, val);
+    this.dirty = true;
+  }
+
+  done() {
+    var value = _.cloneDeep(this.currentValue);
+    value.timeTo = new Date().getTime();
+    this.previousValues.push(value);
+
+    if (this.previousValues.length > 100) {
+      this.previousValues.shift();
+    }
+
+    this.currentValue.timeFrom = new Date().getTime();
+    this.currentValue.timeTo = null;
+    this.dirty = false;
+  }
+}
+
+class UAVWatcher {
+
+  constructor(definitionsStore) {
+    this.definitionsStore = definitionsStore;
+    this.containers = {};
+  }
+
+  objectIdOrName(objectIdOrName) {
+    var objectId;
+
+    if (isNaN(parseInt(objectIdOrName))) {
+      var definition = this.definitionsStore.getDefinitionByName(objectIdOrName);
+      if (_.isUndefined(definition)) {
+        return null;
+      }
+      objectId = definition.id;
+    } else {
+      objectId = objectIdOrName;
+    }
+
+    return objectId;
+  }
+
+  // value is what you get from the payload.data field of an update
+  addOrUpdateUAV(objectIdOrName, value) {
+    var objectId = this.objectIdOrName(objectIdOrName);
+
+    var container = this.containers[objectId];
+    if (container === undefined) {
+      container = new UAVContainer(objectId, value);
+      this.containers[objectId] = container;
+    } else {
+      container.update(value);
+    }
+    return container;
+  }
+
+  forEachDirty(fn) {
+    _.forEach(this.containers, function(container) {
+      if (container.dirty) {
+        fn(container);
+      }
+    });
+  }
+
+  valueForUAV(objectIdOrName) {
+    var objectId = this.objectIdOrName(objectIdOrName);
+
+    var container = this.containers[objectId];
+    return container ? container.currentValue.value : {};
+  }
+}
+
+module.exports = UAVWatcher;
